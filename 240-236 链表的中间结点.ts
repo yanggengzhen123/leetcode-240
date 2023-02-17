cb6d31913a07f50cc25ed3815e78d836f7295afc
yanggengzhen <15017290625@163.com>
2023-02-17 16:32:59 +0800

    20230217-1

---
+++ 240-236 链表的中间结点.ts
@@ -0,0 +1,19 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+// https://leetcode.cn/problems/middle-of-the-linked-list/submissions/403308945/?languageTags=javascript
+function middleNode(head: ListNode | null): ListNode | null {
+    // 快慢指针
+    let slow: ListNode | null = head;
+    let fast: ListNode | null = head;
+    while (fast && fast.next) {
+        slow = slow!.next;
+        fast = fast.next.next;
+    }
+    return slow;
+}
