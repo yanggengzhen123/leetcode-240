c6a758aae453e8a5fae71e013fbe215ae23e7733
yanggengzhen <15017290625@163.com>
2023-03-03 23:57:49 +0800

    20230303-2

--- 240-13 爬楼梯.ts
+++ 240-13 爬楼梯.ts
@@ -1,6 +1,6 @@
 // https://leetcode.cn/problems/climbing-stairs/description/
 function climbStairs(n: number): number {
-    let momo = [1,2]
+    let momo:number[] = [1,2]
     // 1 2 3 5
     for(let i = 2;i < n;i++){
         momo[i] = momo[i - 1] + momo[i - 2]
