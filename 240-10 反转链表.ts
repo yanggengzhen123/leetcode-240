e2bb26e2dec9e9122548e3e7a9c5c87ab47f3976
yanggengzhen <15017290625@163.com>
2023-03-04 18:23:50 +0800

    20230304-2

---
+++ 240-10 反转链表.ts
@@ -0,0 +1,20 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+function reverseList(head: ListNode | null): ListNode | null {
+    // 双指针
+    let cur: ListNode | null = head;
+    let pre: ListNode | null = null;
+    while (cur !== null) {
+        let temp = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = temp;
+    }
+    return pre;
+}
